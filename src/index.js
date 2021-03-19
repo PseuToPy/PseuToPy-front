06cd84817da53c4bbb5177fc5b31f033bbfbb3df
Laurent Yu <laurentyu1997@hotmail.fr>
2021-03-19 16:48:13 +0100

    theme plus some works on components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import 'primeflex/primeflex.css';
-import 'primereact/resources/themes/saga-blue/theme.css';
+// import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
-import './assets/theme/theme.scss';
+import './assets/theme/mytheme/theme.scss';
 import './index.scss';
 
 ReactDOM.render(
