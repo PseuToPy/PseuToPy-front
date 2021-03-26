158dabae5319bc456b9c2f96df469945c38696f5
Hmatico <mathieu.valentin1@orange.fr>
2021-03-26 14:43:37 +0100

    PWA-012 Ajout du thème et utilisation des composants (#1)
    * [TASK][PWA-012] create first component + using a simple menu from Prime
    
    * theme plus some works on components
    
    * some designs on toast
    
    * code editor component

---
+++ src/components/CodeEditor/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Editor from "react-simple-code-editor";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import PropTypes from "prop-types";
+import theme from "prism-react-renderer/themes/github";
+
+import "./CodeEditor.scss";
+
+const CodeEditor = ({ language = "", initialCode = "", readonly = false }) => {
+    const [code, setCode] = useState(initialCode);
+
+    const highlight = rawCode => (
+        <Highlight
+            {...defaultProps}
+            theme={theme}
+            code={rawCode}
+            language={language}
+        >
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                <div className="editor-container">
+                    {tokens.map((line, i) => (
+                        <div
+                            key={i}
+                            {...getLineProps({ line, key: i })}
+                            className="editor-line"
+                        >
+                            <span className="editor-line-number">{i + 1}</span>
+                            <span className="editor-line-content">
+                                {line.map((token, key) => (
+                                    <span
+                                        key={key}
+                                        {...getTokenProps({ token, key })}
+                                    />
+                                ))}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </Highlight>
+    );
+
+    return (
+        <Editor
+            className="editor code"
+            value={code}
+            onValueChange={newCode => setCode(newCode)}
+            highlight={highlight}
+            disabled={readonly}
+            style={{ ...theme.plain }}
+        />
+    );
+};
+
+CodeEditor.propTypes = {
+    language: PropTypes.string,
+    initialCode: PropTypes.string,
+    readonly: PropTypes.bool,
+};
+
+export default CodeEditor;
