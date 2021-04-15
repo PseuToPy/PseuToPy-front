0bed7104f91206a450d394922d908be39bc56568
hmatico <mathieu.valentin1@orange.fr>
2021-04-15 12:39:39 +0200

    [PWA_000] Adding markdown support to text and a template for cgu

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,56 +1,35 @@
 import "./style.scss";
+import { useState, useEffect } from "react";
 import { Panel } from "primereact/panel";
 import { useTranslation } from "react-i18next";
+import Markdown from "markdown-to-jsx";
+import about_en from "./markdown/about_en.md";
+import about_fr from "./markdown/about_fr.md";
 
 const About = () => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const [markdown, setMarkdown] = useState("");
+
+    useEffect(() => {
+        fetch(renderSwitch(i18n.language))
+            .then(res => res.text())
+            .then(text => setMarkdown(text));
+    }, [i18n.language]);
+
+    const renderSwitch = key => {
+        switch (key) {
+            case "fr":
+                return about_fr;
+            default:
+                return about_en;
+        }
+    };
 
     return (
         <div className="p-grid">
             <div className="p-col-2"></div>
             <Panel header={t("about.header")} className="p-col-8 p-shadow-4">
-                <div id="content">
-                    <h2
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_1.header"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_1.1"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_1.2"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_1.3"),
-                        }}
-                    />
-                    <h2
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_2.header"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_2.1"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_2.2"),
-                        }}
-                    />
-                    <p
-                        dangerouslySetInnerHTML={{
-                            __html: t("about:part_2.3"),
-                        }}
-                    />
-                </div>
+                <Markdown children={markdown} />
             </Panel>
             <div className="p-col-2"></div>
         </div>
---
+++ src/components/pages/Cgu/markdown/gcu_en.md
@@ -0,0 +1,27 @@
+# <center>General conditions of use of the site</center>
+
+## <center>Effective as of (insert date)</center>
+
+Les présentes conditions générales d'utilisation(dites «CGU») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par(indiquer le nom du site)et de définir les conditions d’accès et d’utilisation desservicespar «l'Utilisateur».<br  />Les présentes CGU sont accessibles sur le site à la rubrique«CGU».
+
+Les CGU doivent être acceptées par tout Utilisateur souhaitant accéder au site. Elles constituent le contrat entre le site et l'Utilisateur. L’accès au site par l’Utilisateur signifie son acceptation des présentes CGU. Il s’engage désormais à respecter les présentes conditions.<br  />Ou si le site comprend un espace membre: <br  />Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur.Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentesCGUen cochant la case précédant le texte suivant: « Je reconnais avoir lu et compris les CGU et je les accepte».
+
+En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés parle site.(indiquer le nom du site)se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.
+
+### ARTICLE 1 : Mentions légales
+
+### ARTICLE 2 : Accès au site
+
+### ARTICLE 3 : Données personnelles
+
+### ARTICLE 4 : Propriété intellectuelle
+
+### ARTICLE 5 : Responsabilité
+
+### ARTICLE 6 : Liens hypertextes
+
+### ARTICLE 7: Cookies
+
+### ARTICLE 8 : Publication par l’Utilisateur
+
+### ARTICLE (8 ou 9) : Droit applicable et juridiction compétente
